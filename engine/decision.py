013c403c5b908ef8df44dc3000183f0af379b30a
siyer13 <sreedharriyer@gmail.com>
2019-11-09 13:34:11 +0530

    app and decision

--- engine/decision.py
+++ engine/decision.py
@@ -14,7 +14,7 @@ def response_parser(response, firstname, lastname):
             print(team_members)
             for name in team_members:
                 s = s + str(team_members[name])
-            return s[2:-2]
+            return s
         elif 'reporting manager' in query_text and 'know_self' in intent:
             s = ""
             t = requests.get(url + '/getreportingmanager?firstname='+firstname+'&lastname='+lastname)
@@ -22,58 +22,72 @@ def response_parser(response, firstname, lastname):
             for name in rm:
                 s = s + str(rm[name])
             s = s[2:-2]
-            s = 'Reporting Manager for: ' + s
+            s = 'Reporting Manager: ' + s
             return s
         elif 'business unit' in query_text or 'bu' in query_text and 'know_self' in intent:
-            s = "Business Unit is: "
+            s = ""
             t = requests.get(url + '/getbusinessunit?firstname='+firstname+'&lastname='+lastname)
             bu = t.json()
             for name in bu:
                 s = s + str(bu[name])
-            return s[2:-2]
+            s = s[2:-2]
+            s = 'Business Unit is: ' + s
+            return s
         elif 'project' in query_text or 'account' in query_text and 'know_self' in intent:
-            s = "Account is: "
+            s = ""
             t = requests.get(url + '/getprojectname?firstname='+firstname+'&lastname='+lastname)
             pj = t.json()
             for name in pj:
                 s = s + str(pj[name])
-            return s[2:-2]
+            s = s[2:-2]
+            s = 'Account is: ' + s
+            return s
         elif 'doj' in query_text or 'date of joining' in query_text or 'joining date' in query_text or 'hiring' in query_text and 'know_self' in intent:
-            s = "Date of Joining is: "
+            s = ""
             t = requests.get(url + '/getjoiningdate?firstname='+firstname+'&lastname='+lastname)
             doj = t.json()
             for name in doj:
                 s = s + str(doj[name])
-            return s[2:-2]
+            s = s[2:-2]
+            s = 'Date of Joining is: ' + s
+            return s
         elif 'practice lead' in query_text or 'project lead' in query_text and 'know_self' in intent:
-            s = "Practice Lead is: "
+            s = ""
             t = requests.get(url + '/getpractielead?firstname='+firstname+'&lastname='+lastname)
             pl = t.json()
             for name in pl:
                 s = s + str(pl[name])
+            s = s[2:-2]
+            s = 'Practice Lead is: ' + s
             return s
         elif 'location' in query_text or 'place' in query_text and 'know_others' in intent:
-            s = "Location is: "
+            s = ""
             t = requests.get(url + '/location?firstname='+firstname+'&lastname='+lastname)
             loc = t.json()
             for name in loc:
                 s = s + str(loc[name])
+            s = s[2:-2]
+            s = 'Location is: ' + s
             return s
         elif 'largest account' in query_text or 'biggest account' in query_text and 'know_aggr' in intent:
-            s = "Largest account at Enquero is: "
+            s = ""
             t = requests.get(url + '/largestaccount')
             cb = t.json()
             for name in cb:
                 s = s + str(cb[name])
+            s = s[2:-2]
+            s = 'Largest account at Enquero is: ' + s
             return s
 
 
         elif 'business' in query_text or 'head count' in query_text and 'know_aggr' in intent:
-            s = "Business count is: "
+            s = ""
             t = requests.get(url + '/countbybusinesstitle')
             cb = t.json()
             for name in cb:
                 s = s + str(cb[name])
+            s = s[2:-2]
+            s = 'Business count is: ' + s
             return s
 
 
