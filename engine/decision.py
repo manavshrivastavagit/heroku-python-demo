668d31219bd8a5ded6f2608cc8ae4503a90723ee
siyer13 <sreedharriyer@gmail.com>
2019-11-09 11:19:20 +0530

    response parser

--- engine/decision.py
+++ engine/decision.py
@@ -3,13 +3,17 @@ import json
 
 url = 'https://nero-enquero.herokuapp.com'
 def response_parser(response, firstname, lastname):
+    s = "Team members are : "
     query_text = response.query_result.query_text
     fulfillment_text = response.query_result.fulfillment_text
     intent = response.query_result.intent.display_name
     if not fulfillment_text:
         if 'team members' in query_text and 'know_your_team' in intent:
             team_members = requests.get(url+'/getteammembers?firstname='+firstname+'&lastname='+lastname)
-            print(json.dumps(team_members.json()))
-            return "my team members"
+            tm = json.loads(team_members.json())
+            for name in tm:
+                for n in name:
+                    s = s + n
+            return s
     else:
      return fulfillment_text
