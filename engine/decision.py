0141f78e885c682920a8af2984f312aaac222add
siyer13 <sreedharriyer@gmail.com>
2019-11-09 13:25:12 +0530

    app and decision

--- engine/decision.py
+++ engine/decision.py
@@ -8,13 +8,15 @@ def response_parser(response, firstname, lastname):
     intent = response.query_result.intent.display_name
     if not fulfillment_text:
         if 'team members' in query_text and 'know_your_team' in intent:
-            s = "Team members are : "
+            s = ""
             t = requests.get(url+'/getteammembers?firstname='+firstname+'&lastname='+lastname)
             team_members = t.json()
             print(team_members)
             for name in team_members:
                 s = s + str(team_members[name])
-            return s[2:-2]
+            s = s[2:-2]
+            s = 'Team members are : ' + s
+            return s
         elif 'reporting manager' in query_text and 'know_self' in intent:
             s = "Reporting Manager for: "
             t = requests.get(url + '/getreportingmanager?firstname='+firstname+'&lastname='+lastname)
