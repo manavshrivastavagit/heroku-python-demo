f5525e09931bad9e979a74cdc67e9451ad0ef41a
siyer13 <sreedharriyer@gmail.com>
2019-11-09 06:20:44 +0530

    response

--- engine/decision.py
+++ engine/decision.py
@@ -2,5 +2,5 @@ import requests
 
 url = 'https://nero-enquero.herokuapp.com'
 def response_parser(response, firstname, lastname):
-    print(response)
+    print("My RESPONSE ------> ", response)
     #requests.get(url+'/')
