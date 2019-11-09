26e51e37517979e7256dc66bada1c904d062fc2e
siyer13 <sreedharriyer@gmail.com>
2019-11-09 07:04:28 +0530

    response parser

--- engine/decision.py
+++ engine/decision.py
@@ -4,10 +4,8 @@ url = 'https://nero-enquero.herokuapp.com'
 def response_parser(response, firstname, lastname):
     query_text = response.query_result.query_text
     print('---------------' , query_text)
-    fulfillment_text = response.query_result.get('fulfillment_text')
+    fulfillment_text = response['query_result'].get('fulfillment_text')
     if fulfillment_text is None:
         print('no text recived routing')
     else:
         print('---------------', fulfillment_text)
-
-    #requests.get(url+'/')
