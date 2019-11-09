2e54d29774bffbb52fbf711038375860d4324e79
siyer13 <sreedharriyer@gmail.com>
2019-11-09 13:27:57 +0530

    app and decision

--- engine/decision.py
+++ engine/decision.py
@@ -8,22 +8,22 @@ def response_parser(response, firstname, lastname):
     intent = response.query_result.intent.display_name
     if not fulfillment_text:
         if 'team members' in query_text and 'know_your_team' in intent:
-            s = ""
+            s = "Team members are : "
             t = requests.get(url+'/getteammembers?firstname='+firstname+'&lastname='+lastname)
             team_members = t.json()
             print(team_members)
             for name in team_members:
                 s = s + str(team_members[name])
-            s = s[2:-2]
-            s = 'Team members are : ' + s
-            return s
+            return s[2:-2]
         elif 'reporting manager' in query_text and 'know_self' in intent:
-            s = "Reporting Manager for: "
+            s = ""
             t = requests.get(url + '/getreportingmanager?firstname='+firstname+'&lastname='+lastname)
             rm = t.json()
             for name in rm:
                 s = s + str(rm[name])
-            return s[2:-2]
+            s = s[2:-2]
+            s = 'Reporting Manager for: ' + s
+            return s
         elif 'business unit' in query_text or 'bu' in query_text and 'know_self' in intent:
             s = "Business Unit is: "
             t = requests.get(url + '/getbusinessunit?firstname='+firstname+'&lastname='+lastname)
@@ -51,21 +51,21 @@ def response_parser(response, firstname, lastname):
             pl = t.json()
             for name in pl:
                 s = s + str(pl[name])
-            return s[2:-2]
+            return s
         elif 'location' in query_text or 'place' in query_text and 'know_others' in intent:
             s = "Location is: "
             t = requests.get(url + '/location?firstname='+firstname+'&lastname='+lastname)
             loc = t.json()
             for name in loc:
                 s = s + str(loc[name])
-            return s[2:-2]
+            return s
         elif 'largest account' in query_text or 'biggest account' in query_text and 'know_aggr' in intent:
             s = "Largest account at Enquero is: "
             t = requests.get(url + '/largestaccount')
             cb = t.json()
             for name in cb:
                 s = s + str(cb[name])
-            return s[2:-2]
+            return s
 
 
         elif 'business' in query_text or 'head count' in query_text and 'know_aggr' in intent:
@@ -74,7 +74,7 @@ def response_parser(response, firstname, lastname):
             cb = t.json()
             for name in cb:
                 s = s + str(cb[name])
-            return s[2:-2]
+            return s
 
 
 
